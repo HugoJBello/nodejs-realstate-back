5cfa59d84dcb900eb25c61afe613e10b6a9561b6
Hugo J. Bello <hjbello.wk@gmail.com>
2018-11-15 18:02:45 +0100

    fix

--- scraper-logger-nodejs-back/routes/stateExecution.js
+++ scraper-logger-nodejs-back/routes/stateExecution.js
@@ -3,27 +3,32 @@ var StateExecution = require('../models/stateExecution');
 var router = express.Router();
 
 //http://localhost:3001/stateExecution/state-execution-airbnb-scraping/skip=0&limit=2
-router.get('/:db/skip=:skip&limit=:limit',
+router.get('/:db/skip=:skip&limit=:limit&order=:order',
     function (req, res) {
         let skip;
         let limit;
+        let order;
         let db = req.params.db;
         if (req.params.skip) skip = parseInt(req.params.skip, 10)
         if (req.params.limit) limit = parseInt(req.params.limit, 10)
-        stateExecutionFind(res, skip, limit);
+        if (req.params.order) order = req.params.order;
+
+        stateExecutionFind(res, skip, limit, order);
     });
 
-router.get('/limit=:limit',
+router.get('/limit=:limit&order=:order',
     function (req, res) {
         let skip;
         let limit;
+        let order;
         if (req.params.skip) skip = parseInt(req.params.skip, 10)
         if (req.params.limit) limit = parseInt(req.params.limit, 10)
-        stateExecutionFind(res, undefined, limit);
+        if (req.params.order) order = req.params.order;
+        stateExecutionFind(res, undefined, limit, order);
     });
 
-stateExecutionFind = (res, skip, limit) => {
-    StateExecution.find({}).sort({ date: -1 }).limit(limit).skip(skip).exec(function (err, executions) {
+stateExecutionFind = (res, skip, limit, order = -1) => {
+    StateExecution.find({}).sort({ date: order }).limit(limit).skip(skip).exec(function (err, executions) {
         if (err) {
             console.log(err);
             throw err;
