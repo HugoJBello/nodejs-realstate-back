9037ad184ecd50482e9ea81b0acafcf22ca224ce
hugo bello <hjbello.wk@gmail.com>
2018-11-17 13:10:33 +0100

    added features

--- scraper-logger-reactjs-front/src/components/ScrapingExecutions.jsx
+++ scraper-logger-reactjs-front/src/components/ScrapingExecutions.jsx
@@ -5,20 +5,35 @@ class ScrapingExecutions extends Component {
         super(props);
         this.state = {
             dbName: "state-execution-airbnb-scraping",
-            limit: 10,
+            limit: 100,
             skip: 0,
+            maxDateDiff: 1000 * 60 * 20,
             retrievedExec: null
         }
     }
 
     async componentDidMount() {
         const retrievedExec = await getExecutions(this.state.dbName, this.state.limit, this.state.skip);
-        console.log(retrievedExec);
         this.setState({ retrievedExec })
+        this.findOutIfActive();
+        console.log(this.state);
     }
 
     render() {
         return (<div>Scraping executions</div>);
+    }
+
+    findOutIfActive = () => {
+        const retrievedExec = this.state.retrievedExec;
+        const maxDateDiff = this.state.maxDateDiff;
+
+        retrievedExec.map((exec) => {
+            const date = (new Date(exec.date)).getTime();
+            const dateNow = (new Date()).getTime();
+            const dateDiff = dateNow - date
+            exec.isActive = dateDiff < maxDateDiff;
+        });
+
     }
 }
 
