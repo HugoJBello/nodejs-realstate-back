8848edc30378109523266505cfc1910f917ce64f
Hugo J. Bello <hjbello.wk@gmail.com>
2018-11-30 15:39:22 +0100

    fix

--- routes/stateExecutionMysql.js
+++ routes/stateExecutionMysql.js
@@ -34,5 +34,21 @@ router.get('/',
 stateExecutionFindMysql = (res, skip, limit, order = -1, db) => {
     return res.json({});
 }
+
+
+/*
+
+select * from scraping_results left join scraping_pieces_index on scraping_results.piece_id = scraping_pieces_index.piece_id
+order by date_scraped asc
+limit 10
+offset 0;
+
+select  t.last_piece,t.city_name, r.scraping_id, r.app_id, r.device_id, r.date_scraped,  r.average_prize_buy, r.number_of_ads_buy, r.average_prize_rent, r.number_of_ads_rent from (select * from scraping_execution_log left join  scraping_pieces_index on scraping_execution_log.last_piece = scraping_pieces_index.piece_id) t left join scraping_results r on t.last_piece = r.piece_id
+order by r.date_scraped asc
+limit 10
+offset 0;
+
+*/
+
 module.exports = router;
 
