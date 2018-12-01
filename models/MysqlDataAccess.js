e3b7de64f1aee545929e8238c6f725a29a7e45bc
Hugo J. Bello <hjbello.wk@gmail.com>
2018-12-01 11:33:25 +0100

    fix

--- models/MysqlDataAccess.js
+++ models/MysqlDataAccess.js
@@ -71,8 +71,11 @@ module.exports = class MysqlDataAccess {
         }
     }
 
-    async getScrapingExecutionLog(limit,offset, order="asc"){
-        const sql = `select  t.last_piece,t.city_name, r.scraping_id, r.app_id, r.device_id, r.date_scraped,  r.average_prize_buy, r.number_of_ads_buy, r.average_prize_rent, r.number_of_ads_rent from (select * from scraping_execution_log left join  scraping_pieces_index on scraping_execution_log.last_piece = scraping_pieces_index.piece_id) t left join scraping_results r on t.last_piece = r.piece_id
+    async getScrapingExecutionLog(limit,offset, order="desc"){
+        const sql = `select t.scraping_id, t.last_piece,r.date_scraped, r.app_id, r.device_id
+        from (select * from scraping_execution_log 
+        left join  scraping_pieces_index on scraping_execution_log.last_piece = scraping_pieces_index.piece_id) t 
+        left join scraping_results r on t.last_piece = r.piece_id
         order by r.date_scraped ${order}
         limit ${limit}
         offset ${offset};`;
